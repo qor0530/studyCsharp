9700abb15c23214b439350acd1a87c001508679b
qor0530 <qorwhdghk12@gmail.com>
2020-03-02 00:34:39 +0900

    HelloC#

---
+++ StudyCSharp/StudyCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StudyCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법 : HellWorld.exe <이름>");
+                return;
+            }
+            Console.WriteLine("Hello, {0}!\nNice to Meet You!", args[0]);
+        }
+    }
+}
